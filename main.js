802627f2af0dc03f8564b765f49d8d93760d9408
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-05 20:35:10 +0800

    Add main script

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 $(() => {
   console.log("Ready");
-  $("h1").on("click", (e) => {
-    console.log(e);
-  });
+  $("h1")
+    .on("click", (e) => {
+      console.log(e);
+    })
+    .css("font-size");
   console.log("html()", $("h2").html());
   console.log("text()", $("h2").text());
+  console.log("css(property)", $("h2").css("color", "red"));
+  $("h1").css({ color: "green" });
+  $(".box").attr("style", "display: flex");
+  $(".box").css({
+    height: "300px",
+    width: "300px",
+    background: "red",
+    margin: "20px",
+    "margin-top": "50px",
+  });
 });

6bf67f076861cd4afacd506b3e4692f09d56a739
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-05 20:35:21 +0800

    Add lib

--- src/queryLib.ts
+++ src/queryLib.ts
@@ -8,8 +8,10 @@ const $ = (arg: string | CallableFunction) => {
             cb(element);
           });
         });
+
+        return instance;
       },
-      html: () => {
+      html: (): HTMLElement => {
         return instance.elements.length == 1
           ? instance.elements[0]
           : Array.prototype.reduce.call(
@@ -22,6 +24,32 @@ const $ = (arg: string | CallableFunction) => {
       },
       text: (index: number = 0) => {
         return (instance.elements[index] as HTMLElement).innerText;
+      },
+      css: (...args) => {
+        if (typeof (args[0] === "string") && typeof (args[1] === "string")) {
+          instance.elements.forEach((el) => {
+            (el as HTMLElement).style[args[0]] = args[1];
+          });
+        }
+
+        if (typeof args[0] === "object") {
+          instance.elements.forEach((el) => {
+            for (let rule in args[0]) {
+              (el as HTMLElement).style[rule] = args[0][rule];
+            }
+          });
+        }
+
+        return instance;
+      },
+      attr: (...args: [attribute: string, value?: any]) => {
+        if (args.length == 1) return instance.elements[0].getAttribute(args[0]);
+
+        instance.elements.forEach((el) => {
+          el.setAttribute(args[0], args[1]);
+        });
+
+        return instance;
       },
     };
 

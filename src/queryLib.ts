bec98866ce79872c4a6ff31141b28975ab86cb32
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-10 10:28:02 +0800

    Added show, hide, addclass,removeclass

--- src/queryLib.ts
+++ src/queryLib.ts
@@ -2,15 +2,17 @@ const $ = (arg: string | CallableFunction) => {
   if (typeof arg === "string") {
     const instance = {
       elements: document.querySelectorAll(arg),
-      on: (e, cb) => {
+      // Add event handler
+      on: (e, callback: Function) => {
         instance.elements.forEach((element) => {
           element.addEventListener(e, (event: Event) => {
-            cb(event);
+            callback(event);
           });
         });
 
         return instance;
       },
+      // Returns html
       html: (): HTMLElement => {
         return instance.elements.length == 1
           ? instance.elements[0]
@@ -22,9 +24,11 @@ const $ = (arg: string | CallableFunction) => {
               ""
             );
       },
+      // Returns text
       text: (index: number = 0) => {
         return (instance.elements[index] as HTMLElement).innerText;
       },
+      // Sets css properties
       css: (...args) => {
         if (typeof (args[0] === "string") && typeof (args[1] === "string")) {
           instance.elements.forEach((el) => {
@@ -42,6 +46,7 @@ const $ = (arg: string | CallableFunction) => {
 
         return instance;
       },
+      // Sets attributes
       attr: (...args: [attribute: string, value?: any]) => {
         if (args.length == 1) return instance.elements[0].getAttribute(args[0]);
 
@@ -51,25 +56,49 @@ const $ = (arg: string | CallableFunction) => {
 
         return instance;
       },
-      hide: (duration = 1, easing = "ease-in-out", delay = 1) => {
+      // Hides element
+      hide: () => {
+        instance.elements.forEach((el) => {
+          (el as HTMLElement).style.display = "none";
+        });
+      },
+      // Reveals element
+      show: () => {
+        instance.elements.forEach((el) => {
+          (el as HTMLElement).style.display = "block";
+        });
+      },
+      // Adds className
+      addClass: (className: string) => {
+        instance.elements.forEach((el) => {
+          (el as HTMLElement).classList.add(className);
+        });
+      },
+      // removes className
+      removeClass: (className: string) => {
+        instance.elements.forEach((el) => {
+          (el as HTMLElement).classList.remove(className);
+        });
+      },
+      // Toggles className of elements
+      toggleClass: (className: string, addOrRemove) => {
+        instance.elements.forEach((el) => {
+          (el as HTMLElement).classList.toggle(className, addOrRemove);
+        });
+      },
+      slideUp: (
+        duration: number = 0.5,
+        easing: string = "ease",
+        delay: number = 0
+      ) => {
         instance.elements.forEach((el) => {
           (
             el as HTMLElement
           ).style.transition = `${duration}s ${easing} ${delay}s`;
-          (el as HTMLElement).style.display = "none";
+          (el as HTMLElement).style.transform = `translateY(-100vh)`;
           (el as HTMLElement).style.opacity = "0";
         });
       },
-      show: (duration = 1, easing = "ease-in-out", delay = 1) => {
-        instance.elements.forEach((el) => {
-          (
-            el as HTMLElement
-          ).style.transition = `${duration}s ${easing} ${delay}s`;
-          (el as HTMLElement).style.display = "block";
-          (el as HTMLElement).style.opacity = "1";
-        });
-      },
-      toggleClass: () => {},
     };
 
     return instance;

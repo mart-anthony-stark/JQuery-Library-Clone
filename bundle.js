675b37fefee67fff8c6b1e399fe0dbc978a09bd9
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-10 10:27:01 +0800

    bundle file

--- bundle.js
+++ bundle.js
@@ -2,14 +2,16 @@ var $ = function (arg) {
     if (typeof arg === "string") {
         var instance_1 = {
             elements: document.querySelectorAll(arg),
-            on: function (e, cb) {
+            // Add event handler
+            on: function (e, callback) {
                 instance_1.elements.forEach(function (element) {
                     element.addEventListener(e, function (event) {
-                        cb(event);
+                        callback(event);
                     });
                 });
                 return instance_1;
             },
+            // Returns html
             html: function () {
                 return instance_1.elements.length == 1
                     ? instance_1.elements[0]
@@ -17,10 +19,12 @@ var $ = function (arg) {
                         return html + (node.outerHTML || node.nodeValue);
                     }, "");
             },
+            // Returns text
             text: function (index) {
                 if (index === void 0) { index = 0; }
                 return instance_1.elements[index].innerText;
             },
+            // Sets css properties
             css: function () {
                 var args = [];
                 for (var _i = 0; _i < arguments.length; _i++) {
@@ -40,6 +44,7 @@ var $ = function (arg) {
                 }
                 return instance_1;
             },
+            // Sets attributes
             attr: function () {
                 var args = [];
                 for (var _i = 0; _i < arguments.length; _i++) {
@@ -52,27 +57,46 @@ var $ = function (arg) {
                 });
                 return instance_1;
             },
-            hide: function (duration, easing, delay) {
-                if (duration === void 0) { duration = 1; }
-                if (easing === void 0) { easing = "ease-in-out"; }
-                if (delay === void 0) { delay = 1; }
+            // Hides element
+            hide: function () {
+                instance_1.elements.forEach(function (el) {
+                    el.style.display = "none";
+                });
+            },
+            // Reveals element
+            show: function () {
+                instance_1.elements.forEach(function (el) {
+                    el.style.display = "block";
+                });
+            },
+            // Adds className
+            addClass: function (className) {
+                instance_1.elements.forEach(function (el) {
+                    el.classList.add(className);
+                });
+            },
+            // removes className
+            removeClass: function (className) {
+                instance_1.elements.forEach(function (el) {
+                    el.classList.remove(className);
+                });
+            },
+            // Toggles className of elements
+            toggleClass: function (className, addOrRemove) {
+                instance_1.elements.forEach(function (el) {
+                    el.classList.toggle(className, addOrRemove);
+                });
+            },
+            slideUp: function (duration, easing, delay) {
+                if (duration === void 0) { duration = 0.5; }
+                if (easing === void 0) { easing = "ease"; }
+                if (delay === void 0) { delay = 0; }
                 instance_1.elements.forEach(function (el) {
                     el.style.transition = "".concat(duration, "s ").concat(easing, " ").concat(delay, "s");
-                    el.style.display = "none";
+                    el.style.transform = "translateY(-100vh)";
                     el.style.opacity = "0";
                 });
-            },
-            show: function (duration, easing, delay) {
-                if (duration === void 0) { duration = 1; }
-                if (easing === void 0) { easing = "ease-in-out"; }
-                if (delay === void 0) { delay = 1; }
-                instance_1.elements.forEach(function (el) {
-                    el.style.transition = "".concat(duration, "s ").concat(easing, " ").concat(delay, "s");
-                    el.style.display = "block";
-                    el.style.opacity = "1";
-                });
-            },
-            toggleClass: function () { }
+            }
         };
         return instance_1;
     }
